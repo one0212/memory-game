0167467e5e07241cc37129ebc7540d915b0cf5d0
one0212 <feliciajiang12@gmail.com>
2019-12-11 12:17:51 +0800

    added matching logic

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
-let allCards = document.querySelectorAll(".card");
-[...allCards].forEach(card => {
-  card.addEventListener("click", function() {
-    this.classList.add("turn");
-  });
-});
+const allCards = document.querySelectorAll(".memory-card");
+
+let hasFlippedCard = false;
+let lockBoard = false
+let firstCard, secondCard;
+
+function flipCard() {
+  if (lockBoard) return
+  if (this === firstCard) return;
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    // 第一次點擊
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+  // 第二次點擊
+  secondCard = this;
+  console.log(firstCard, secondCard, hasFlippedCard);
+  checkForMatch();
+}
+
+function checkForMatch() {
+  // 判斷是否吻合
+  // console.log(firstCard.dataset.img, secondCard.dataset.img)
+  let isMatch = firstCard.dataset.img === secondCard.dataset.img;
+  isMatch ? disableCards() : unFlipCards();
+}
+function disableCards() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+
+  resetBoard()
+}
+
+function unFlipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+
+    resetBoard()
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false]
+  [firstCard, secondCard] = [null, null]
+}
+
+allCards.forEach(card => card.addEventListener("click", flipCard));
